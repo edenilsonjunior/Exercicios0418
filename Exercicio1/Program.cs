98d25372410ad17bb3e54343f033a5f32e15cfcb
Edenilson <edenilsonju10@gmail.com>
2024-04-18 15:42:04 -0300

    Add project files.

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("\n-----Numeros pares e impares-----\n");
+
+int inicio = 0, fim = 0, aux;
+
+Console.Write("Digite o numero inicial: ");
+inicio = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o numero final: ");
+fim = int.Parse(Console.ReadLine());
+
+aux = inicio;
+aux++;
+Console.WriteLine("Numeros pares:");
+while (aux < fim)
+{
+    if (aux % 2 == 0){
+        Console.Write($"{aux} ");
+    }
+    aux++;
+}
+
+aux = inicio;
+aux++;
+Console.WriteLine("\nNumeros impares:");
+while (aux < fim)
+{
+    if (aux % 2 != 0)
+    {
+        Console.Write($"{aux} ");
+    }
+    aux++;
+}
+
+Console.WriteLine("sHOW");
+Console.WriteLine("\nPressione qualquer tecla para continuar...");
+Console.ReadKey();
---
+++ Exercicio2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("\n-----Tabuada-----\n");
+int num = 0;
+
+do
+{
+    Console.Write("Digite um numero inteiro positivo: ");
+    num = int.Parse(Console.ReadLine());
+} while (num < 0);
+
+for(int i = 1; i<=10; i++)
+{
+    Console.WriteLine($"{num}x{i}={num*i}");
+}
+
+Console.WriteLine("Pressione qualquer tecla para continuar...");
+Console.ReadKey();
